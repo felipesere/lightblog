bafd708537239d8e5e0ce0e15fa6ed59e93b4238
Felipe Sere <felipesere@gmail.com>
2015-03-07 11:05:39 +0000

    Adds RSpec

---
+++ spec/index_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+require 'index/show'
+
+RSpec.describe LightBlog::Index::Show do
+  it 'tells the truth' do
+    expect(true).to be_truthy
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,7 @@
+require_relative '../app.rb'
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+end
