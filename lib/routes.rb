557de1403c10b267d8a0dacf4340f714d918f3cb
Felipe Sere <felipesere@gmail.com>
2015-03-08 13:47:10 +0000

    Replace hard coded html with a dummy post object

--- lib/routes.rb
+++ lib/routes.rb
@@ -1,7 +1,22 @@
 module LightBlog
   class Routes < Sinatra::Application
     get '/' do
-      erb :index, layout: true
+      post = Post.new(title: 'Ripples in Abstraction',
+                   subtitle: 'When things go gently wrong',
+                   author: 'Felipe Sere',
+                   date: 'October 23, 2014')
+      erb :index, layout: true, locals: { post: post }
     end
+  end
+
+  class Post
+    def initialize(params)
+      @title = params[:title]
+      @subtitle = params[:subtitle]
+      @author = params[:author]
+      @date = params[:date]
+    end
+
+    attr_reader :title, :subtitle, :author, :date
   end
 end
