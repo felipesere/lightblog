3751d26e6bda2ec84d72c035502753309dd6599f
Felipe Sere <felipesere@gmail.com>
2015-05-29 15:46:32 +0100

    Unify app and routes to make easier

--- app.rb
+++ app.rb
@@ -8,11 +8,15 @@ require 'sinatra/content_for'
 require 'routes'
 require 'filesystem'
 require 'posts/repository'
+require 'sinatra/named_routes'
+require 'web/tags'
+require 'web/page'
 
 module LightBlog
   class App < Sinatra::Application
-
     Tilt.register Tilt::ERBTemplate, 'html.erb'
+    register Sinatra::NamedRoutes
+    POST_PER_PAGE = 5
 
     configure do
       disable :method_override
@@ -23,9 +27,72 @@ module LightBlog
       set :partial_template_engine, :erb
     end
 
-    fs = LightBlog::Filesystem.new("./posts")
-    repo = LightBlog::Posts::Repository.new(fs)
-    use  LightBlog::Routes, repo
 
+    map :post, '/posts/:slug'
+
+    get '/' do
+      posts = repository.all
+      current_page = Web::Page.new(posts, page, POST_PER_PAGE)
+
+      erb :index, locals: { posts: current_page.filter, page: current_page }
+    end
+
+    get :post do
+      post  = repository.find_by_slug(slug)
+      if post
+        newer = repository.newer(post)
+        older = repository.older(post)
+        erb :post, locals: { post: post, newer: newer, older: older}
+      else
+        erb :not_found
+      end
+    end
+
+    get '/about' do
+      erb :about
+    end
+
+    get '*' do
+      erb :not_found
+    end
+
+    helpers do
+      def post_url(slug)
+        "/posts/#{slug}"
+      end
+
+      def css_tag(*names)
+        tags.css(*names)
+      end
+
+      def js_tag(*names)
+        tags.js(*names)
+      end
+
+      def image_tag(names, options={})
+        tags.image(names, options)
+      end
+    end
+
+    private
+      def tags
+        Web::Tags.new
+      end
+
+      def slug
+        params[:slug]
+      end
+
+      def page
+        @params.fetch("page", 0).to_i
+      end
+
+      def repository
+        @repository ||= LightBlog::Posts::Repository.new(fs)
+      end
+
+      def fs
+        LightBlog::Filesystem.new("./posts")
+      end
   end
 end
